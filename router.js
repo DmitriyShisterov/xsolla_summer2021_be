3ff859110c41cac2c5e94b34821173b85a1e706a
Dmitry <Dmishisterov@gmail.com>
2021-08-16 17:47:34 +0500

    edit router

--- router.js
+++ router.js
@@ -2,7 +2,6 @@ import Router from "express";
 import ProductController from "./controllers/ProductController.js";
 
 const router = new Router();
-
 router.post("/v1/products", ProductController.create);
 router.get("/v1/products/id/:id", ProductController.read);
 router.get("/v1/products/sku/:sku", ProductController.read);
