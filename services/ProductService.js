f058cd885a61e218fe486da669c9989fa76334c6
Dmitry <Dmishisterov@gmail.com>
2021-08-16 17:40:09 +0500

    Reformat ProductController

--- services/ProductService.js
+++ services/ProductService.js
@@ -3,47 +3,43 @@ class ProductService {
     constructor() {}
     create(params) {
         const { sku, type, name, price } = params;
-        let product = {};
-        return (product = Product.create({ sku, type, name, price }));
+        return Product.create({ sku, type, name, price });
     }
     read(params) {
-        let product = {};
         switch (Object.keys(params)[0]) {
             case "id":
-                return (product = Product.findById(params.id));
+                return Product.findById(params.id);
                 break;
             case "sku":
-                return (product = Product.find(params));
+                return Product.find(params);
                 break;
             default:
-                return (product = Product.find(params));
+                return Product.find(params);
                 break;
         }
     }
     update(params) {
-        let product = {};
         if (params._id) {
-            return (product = Product.findByIdAndUpdate(params._id, params, {
+            return Product.findByIdAndUpdate(params._id, params, {
                 new: true,
-            }));
+            });
         }
         if (params.sku) {
-            return (product = Product.findOneAndUpdate(params.sku, params, {
+            return Product.findOneAndUpdate(params.sku, params, {
                 new: true,
-            }));
+            });
         }
         if (!params._id && !params.sku) {
-            return (product = { message: "Content for update not found" });
+            return { message: "Content for update not found" };
         }
     }
     delete(params) {
-        let product = {};
         switch (Object.keys(params)[0]) {
             case "id":
-                return (product = Product.findByIdAndDelete(params.id));
+                return Product.findByIdAndDelete(params.id);
                 break;
             case "sku":
-                return (product = Product.findOneAndDelete(params));
+                return Product.findOneAndDelete(params);
                 break;
         }
     }
