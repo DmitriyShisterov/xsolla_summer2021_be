90d418a91b91c0a53c9c9f3f468f68c5b5f848c3
Dmitry <Dmishisterov@gmail.com>
2021-08-26 15:36:04 +0500

    feature/client-server_auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,7 @@
 import User from "../models/User.js";
 import Role from "../models/Role.js";
-import Bcrypt from "bcryptjs";
 import validator from "express-validator";
-import TokenService from "../services/TokenService.js";
+import AuthService from "../services/AuthService.js";
 
 class authController {
     async registration(req, res) {
@@ -11,22 +10,9 @@ class authController {
             if (!errors.isEmpty()) {
                 return res.status(400).json({ message: "Registration error", errors });
             }
-            const { userName, userPassword } = req.body;
-            const candidate = await User.findOne({ userName });
-            if (candidate) {
-                return res.status(400).json({ message: "User already exist" });
-            }
-            const salt = Bcrypt.genSaltSync(7);
-            const hashUserPassword = Bcrypt.hashSync(userPassword, salt);
-            const uRole = await Role.findOne({ value: "USER" });
-            const user = new User({
-                userName,
-                userPassword: hashUserPassword,
-                userRole: [uRole.value],
-                isActivated: true,
-            });
-            await user.save();
-            return res.json({ message: "User was registred" });
+            const authService = new AuthService();
+            const answer = await authService.registration(req.body);
+            return res.json(answer);
         } catch (e) {
             console.log(e);
             res.status(400).json({ message: "Registration error" });
@@ -34,24 +20,13 @@ class authController {
     }
     async login(req, res) {
         try {
-            const { userName, userPassword } = req.body;
-            const user = await User.findOne({ userName });
-            console.log("user:", user);
-            if (!user) {
-                return res.status(400).json({ messahe: "User not found" });
-            }
-            const validPassword = Bcrypt.compareSync(userPassword, user.userPassword);
-            if (!validPassword) {
-                return res.status(400).json({ messahe: "Password incorrect" });
-            }
-            const tokenService = new TokenService();
-            const tokens = tokenService.generateTokens(user._id, user.userRole);
-            await tokenService.saveTokens(user._id, tokens.refreshToken);
-            res.cookie("refreshToken", tokens.refreshToken, {
+            const authService = new AuthService();
+            const answer = await authService.login(req.body);
+            res.cookie("refreshToken", answer.refreshToken, {
                 maxAge: 30 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
             });
-            return res.json({ ...tokens });
+            return res.json({ ...answer });
         } catch (e) {
             console.log(e);
             res.status(400).json({ message: "Login error" });
@@ -61,8 +36,9 @@ class authController {
         try {
             const users = await User.find();
             res.status(200).json(users);
-            res.json("server work");
-        } catch (e) {}
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
 
--- services/authService.js
+++ services/authService.js
@@ -1,5 +1,41 @@
 import User from "../models/User.js";
+import TokenService from "./TokenService.js";
+import Role from "../models/Role.js";
+import Bcrypt from "bcryptjs";
 class AuthService {
     constructor() {}
+    async registration(params) {
+        const { userName, userPassword } = params;
+        const candidate = await User.findOne({ userName });
+        if (candidate) {
+            return { message: "User already exist" };
+        }
+        const salt = Bcrypt.genSaltSync(7);
+        const hashUserPassword = Bcrypt.hashSync(userPassword, salt);
+        const uRole = await Role.findOne({ value: "USER" });
+        const user = new User({
+            userName,
+            userPassword: hashUserPassword,
+            userRole: [uRole.value],
+            isActivated: true,
+        });
+        await user.save();
+        return { message: "User was created" };
+    }
+    async login(params) {
+        const { userName, userPassword } = params;
+        const user = await User.findOne({ userName });
+        if (!user) {
+            return { message: "User not found" };
+        }
+        const validPassword = Bcrypt.compareSync(userPassword, user.userPassword);
+        if (!validPassword) {
+            return { message: "Password incorrect" };
+        }
+        const tokenService = new TokenService();
+        const tokens = tokenService.generateTokens(user._id, user.userRole);
+        tokenService.saveTokens(user._id, tokens.refreshToken);
+        return tokens;
+    }
 }
-export default authService;
+export default AuthService;
